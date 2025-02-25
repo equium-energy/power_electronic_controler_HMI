43d497b289331f7060998e9c382e8c812782d1e6
corentincazes-equium <corentin.cazes@equium.fr>
2025-02-25 11:10:29 +0100

    :art: Common function to complete for readable code

---
+++ front/common_fct.py
@@ -0,0 +1,12 @@
+from PySide6 import QtWidgets
+import serial
+
+
+def refresh_ports(element: QtWidgets.QComboBox) -> None:
+    """Refresh the port"""
+    ports = [port.device for port in serial.tools.list_ports.comports()]
+    element.clear()  # Clear previous values
+    element.addItems(ports)  # Add new values
+    # Set the first item as the default selection if there are available ports
+    if ports:
+        element.setCurrentIndex(0)
